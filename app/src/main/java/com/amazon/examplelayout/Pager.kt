f7dc675a339c1071ac2c81fb99c200d5749949c7
namphph <nampp@foxcode.org>
2025-08-27 15:15:37 +0700

    fix update horizontal pager

--- app/src/main/java/com/amazon/examplelayout/Pager.kt
+++ app/src/main/java/com/amazon/examplelayout/Pager.kt
@@ -1,27 +1,144 @@
 package com.amazon.examplelayout
 
+import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.PagerDefaults
+import androidx.compose.foundation.pager.PagerSnapDistance
 import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.lerp
 import com.amazon.examplelayout.ui.theme.Purple40
+import kotlinx.coroutines.launch
+import kotlin.math.absoluteValue
 
 @Composable
 fun horizontalPager(){
-    val pagerState = rememberPagerState(pageCount = {
-        10
-    })
-    HorizontalPager(state =  pagerState, modifier = Modifier.padding(20f.dp).background(Purple40)) {
-        Text(
-            text = "Hello"+ it.toString(),
-            modifier = Modifier.fillMaxWidth()
+    Box(modifier = Modifier.fillMaxSize()) {
+        val pagerState = rememberPagerState(pageCount = {
+            10
+        })
+        LaunchedEffect(pagerState) {
+            // Collect from the a snapshotFlow reading the currentPage
+            snapshotFlow { pagerState.currentPage }.collect { page ->
+                // Do something with each page change, for example:
+                // viewModel.sendPageSelectedEvent(page)
+                Log.d("Page change", "Page changed to $page")
+            }
+        }
+        val fling = PagerDefaults.flingBehavior(
+            state = pagerState,
+            pagerSnapDistance = PagerSnapDistance.atMost(10)
         )
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20f.dp)
+                .background(Purple40),
+            pageSize = threePagesPerViewport,
+            contentPadding = PaddingValues(start = 64.dp, end = 64.dp),
+            flingBehavior = fling
+        ) { page ->
+            Card(
+                Modifier
+                    .size(200.dp)
+                    .graphicsLayer {
+                        // Calculate the absolute offset for the current page from the
+                        // scroll position. We use the absolute value which allows us to mirror
+                        // any effects for both directions
+                        val pageOffset = (
+                                (pagerState.currentPage - page) + pagerState
+                                    .currentPageOffsetFraction
+                                ).absoluteValue
+
+                        // We animate the alpha, between 50% and 100%
+                        alpha = lerp(
+                            start = 0.5f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        )
+                    }
+            ) {
+                Text(
+                    text = "Hello" + page.toString(),
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+        val couroutineScope = rememberCoroutineScope()
+        Button(
+            onClick = {
+                // launch coroutine để scroll pager
+                couroutineScope.launch {
+                    pagerState.animateScrollToPage(5)
+                }
+            },
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 20f.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Magenta,  // màu nền Button
+                contentColor = Color.White       // màu chữ bên trong Button
+            )
+        ) {
+            Text("Jump to Page 5")
+        }
+
+        Row(
+            Modifier
+                .wrapContentHeight()
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 50.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            repeat(pagerState.pageCount) { iteration ->
+                val color = if (pagerState.currentPage == iteration) Color.DarkGray else Color.LightGray
+                Box(
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .clip(CircleShape)
+                        .background(color)
+                        .size(16.dp)
+                )
+            }
+        }
+    }
+}
+
+private val threePagesPerViewport = object : PageSize {
+    override fun Density.calculateMainAxisPageSize(
+        availableSpace: Int,
+        pageSpacing: Int
+    ): Int {
+        return (availableSpace - 2 * pageSpacing) / 3
     }
 }
 
